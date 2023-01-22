a0b5d2dc632431bfd4d45e50cb8c692667732526
Subodh Rajpopat <subodhr2001@gmail.com>
2023-01-22 23:59:51 +0530

    host port issue fixed

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
+app.add_middleware(CORSMiddleware, allow_origins=['http://localhost:3000'],
+allow_methods = ['*'],
+allow_headers = ['*'])
+
 redis = get_redis_connection(
-    host = "redis-17159.c301.ap-south-1-1.ec2.cloud.redislabs.com:17159",
-    port = 11844,
+    host = "redis-17159.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    port = 17159,
     password = "fzTahPHzkpWgTtKmNMCOB1BIvdgOrRLL",
     decode_responses = True
 )
@@ -20,4 +25,4 @@ class Product(HashModel):
 
 @app.get("/products")
 def all():
-    return []
+    return Product.all_pks()
